272bf086e91500a81a55ecfd0a6af2f5e5ffbc3c
Piotr Szul <piotr.szul@csiro.au>
2024-10-02 14:14:20 +1000

    Adding knitted version of the study for both pathling and aidbox outputs.

--- scripts/knit_all.sh
+++ scripts/knit_all.sh
@@ -15,9 +15,12 @@ knit_rmd() {
   local input_file=$1
   local output_file=$2
   local output_dir=$3
-  Rscript -e "rmarkdown::render('${input_file}', output_format = 'html_document', output_file = '${output_file}', output_dir = '${output_dir}')"
+  local params=${4:-"NULL"}
+  Rscript -e "rmarkdown::render('${input_file}', output_format = 'html_document', output_file = '${output_file}', output_dir = '${output_dir}', params=${params})"
 }
 
 # Call the function with the desired parameters
-knit_rmd "${R_DIR}/compare.Rmd" "compare.html" "${OUTPUT_DIR}"
-knit_rmd "${R_DIR}/study.Rmd" "study.html" "${OUTPUT_DIR}"
+knit_rmd "${R_DIR}/compare.Rmd" "compare_ptl_vs_aidbox.html" "${OUTPUT_DIR}"
+knit_rmd "${R_DIR}/study.Rmd" "study_ptl.html" "${OUTPUT_DIR}" "list(dataset='_data/ptl_mimic-2.2')"
+knit_rmd "${R_DIR}/study.Rmd" "study_aidbox.html" "${OUTPUT_DIR}" "list(dataset='_data/aidbox_mimic-2.2')"
+
