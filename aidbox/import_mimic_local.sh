ff55af90465828695d27534678234ac5d6a5d090
Piotr Szul <piotr.szul@csiro.au>
2024-10-04 11:43:17 +1000

    Removing internal items.

--- aidbox/import_mimic_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-BASEDIR=$(dirname "$0")
-
-"${BASEDIR}/bin"/import_mimic.py \
-  --mimic-ds-dir "work/mimic4/mimic-iv-on-fhir-demo-1.1.3_af91a8f/fhir" \
-  --auth-username "basic" \
-  --auth-password "secret"
